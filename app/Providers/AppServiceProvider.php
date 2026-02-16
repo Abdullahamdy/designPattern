b008aee3ae4d6c9ca3c3015e02e657186c794b21
abdullahamdy <abdullahhamdy29@gmail.com>
2026-02-16 09:26:29 +0200

    fix confilect

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        //test featured
         $this->app->singleton(NewsFetcherPool::class);
     }
 
